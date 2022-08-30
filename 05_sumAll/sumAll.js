2dd3aa008866c1c472cbd66c4cc824bc4dfeb6cb
Jacob Allen <jallen1887@gmail.com>
2022-08-30 17:30:09 -0400

    Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let bigNum = num2;
+    let smallNum = num1;
+    let result = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    else if (typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        return 'ERROR';
+    }
+    else {
+        if (num1 > num2) {
+            bigNum = num1;
+            smallNum = num2;
+        }
+        for (let i = smallNum; smallNum <= bigNum; smallNum++){
+            result += smallNum;
+        }
+    }
+
+    return result;
 
 };
 
