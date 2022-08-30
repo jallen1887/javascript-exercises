b38cfd036944dc33c19d0154999a0d2339715dd6
Jacob Allen <jallen1887@gmail.com>
2022-08-30 16:40:03 -0400

    Add solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let charArray = text.split('');
+    charArray.reverse();
+    return charArray.join('');
 };
 
 // Do not edit below this line
